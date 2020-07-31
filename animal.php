17df0094950f3180c30141f01f8b8ffb3e069f30
denibro <denisdewdew@gmail.com>
2020-07-31 09:02:21 +0700

    mulai lagi

---
+++ animal.php
@@ -0,0 +1,22 @@
+<?php  
+
+
+class Animal
+{
+
+	public $legs = 2;
+	public $cold_blooded = false;
+	
+	public function __construct($nama)
+	{
+		$this->name = $nama;
+		// $this->legs;
+		// $this->cold_blooded;
+
+	}
+	// function getName(){
+	// 	echo this->nama;
+	// }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>dew</title>
-</head>
-<body>
+<?php  
+require 'animal.php';
 
-</body>
-</html>
+$sheep = new Animal("shaun");
+
+echo $sheep->name; // "shaun"
+echo "<br>";
+echo $sheep->legs; // 2
+echo "<br>";
+echo $sheep->cold_blooded // false
+
+// NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
+
+?>
